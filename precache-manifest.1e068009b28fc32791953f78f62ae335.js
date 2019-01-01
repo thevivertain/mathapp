334a88b5b5edaa636b2b5b40fde103857c70ef74
William <deeanddragons@gmail.com>
2018-12-31 18:53:44 -0800

    Updates

---
+++ precache-manifest.1e068009b28fc32791953f78f62ae335.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6ba7b6fadd3c80c6ab79",
+    "url": "/mathapp/static/css/main.19899a3d.chunk.css"
+  },
+  {
+    "revision": "6ba7b6fadd3c80c6ab79",
+    "url": "/mathapp/static/js/main.6ba7b6fa.chunk.js"
+  },
+  {
+    "revision": "f354b7ecc3e1b7fbd378",
+    "url": "/mathapp/static/js/1.f354b7ec.chunk.js"
+  },
+  {
+    "revision": "5b812f687fa38635ef73",
+    "url": "/mathapp/static/js/runtime~main.5b812f68.js"
+  },
+  {
+    "revision": "96f9a956ddef23d708ad12aec912f5d8",
+    "url": "/mathapp/index.html"
+  }
+];
